b9ebcad8fc49d6eb01ecf8d2bfcdafd3783f10ef
= <=>
2023-01-05 13:20:05 -0600

    Validando datos del formulario

--- app/mail.py
+++ app/mail.py
@@ -1,5 +1,5 @@
 from flask import(
-    Blueprint, render_template
+    Blueprint, render_template, request, flash
 )
 
 from app.db import get_db
@@ -18,4 +18,25 @@ def index():
 
 @bp.route('/create',methods = ['GET','POST'])
 def create():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        subject = request.form.get('subject')
+        content = request.form.get('content')
+        errors = []
+
+        if not email:
+            errors.append('Email obligatorio')
+        
+        if not subject:
+            errors.append('Asunto obligatorio')
+        
+        if not content:
+            errors.append('Contenido obligatorio')
+        
+        if len(errors) == 0:
+            pass
+        else:
+            for error in errors:
+                flash(error)
+
     return render_template('mails/create.html')
